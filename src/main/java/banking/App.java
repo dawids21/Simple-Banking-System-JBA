8b2399b5fef16a5dc920d6237113b36942d63b5c
dawids21 <dawid.stasiak21@gmail.com>
2020-08-14 14:54:56 +0200

    Create necessary objects

--- src/main/java/banking/App.java
+++ src/main/java/banking/App.java
@@ -1,12 +1,20 @@
 package banking;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class App {
 
-    private static AppStates state;
+    private static final String IIN = "400000";
 
     public static void main(String[] args) {
-        state = AppStates.MENU;
-        System.out.println("Hello World!");
+        var state = AppStates.MENU;
+        final var accounts = new HashMap<Card, Account>();
+        final var generator = new CardGenerator(IIN);
+        final var validator = new CardValidator(accounts.keySet());
+        final var input = new Scanner(System.in);
+        Account loggedAccount = null;
+
     }
 
     private enum AppStates {
