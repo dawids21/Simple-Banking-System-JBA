282a233798aa1cac7f173006c953460a78bfca5d
dawids21 <dawid.stasiak21@gmail.com>
2020-08-14 14:22:57 +0200

    Write tests and stubs for card validation

---
+++ src/main/java/banking/CardValidator.java
@@ -0,0 +1,18 @@
+package banking;
+
+import java.util.HashSet;
+
+public class CardValidator {
+
+    private final HashSet<Card> cards;
+
+    public CardValidator(HashSet<Card> cards) {
+        //TODO implement CardValidator
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public boolean valid(String cardNumber, String cardPin) {
+        //TODO implement valid
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
