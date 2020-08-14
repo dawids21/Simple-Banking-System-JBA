b0299e3b3e3299be81e589587b3e31fa065316b9
dawids21 <dawid.stasiak21@gmail.com>
2020-08-14 14:22:33 +0200

    Add app states

--- src/main/java/banking/App.java
+++ src/main/java/banking/App.java
@@ -2,7 +2,14 @@ package banking;
 
 public class App {
 
+    private static AppStates state;
+
     public static void main(String[] args) {
+        state = AppStates.MENU;
         System.out.println("Hello World!");
+    }
+
+    private enum AppStates {
+        MENU, LOGGED
     }
 }
