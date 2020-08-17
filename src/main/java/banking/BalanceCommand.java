fbf780f78037ece6a75c047864608f6fbc9fa69e
dawids21 <dawid.stasiak21@gmail.com>
2020-08-17 13:57:10 +0200

    Create class BalanceCommand

---
+++ src/main/java/banking/BalanceCommand.java
@@ -0,0 +1,10 @@
+package banking;
+
+public class BalanceCommand implements Command {
+
+    @Override
+    public void execute() {
+        //TODO implement execute
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
