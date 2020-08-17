b9b47470f5951d652a60bdaa0c5d4c335a56f1d9
dawids21 <dawid.stasiak21@gmail.com>
2020-08-17 13:10:48 +0200

    Write CommandInvoker

---
+++ src/main/java/banking/CommandInvoker.java
@@ -0,0 +1,22 @@
+package banking;
+
+public class CommandInvoker {
+
+    private Command command;
+
+    public CommandInvoker() {
+        command = null;
+    }
+
+    public Command getCommand() {
+        return command;
+    }
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void execute() {
+        command.execute();
+    }
+}
