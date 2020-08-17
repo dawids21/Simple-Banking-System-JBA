2db568b4846a90373a36190cf53e77cf03a19b90
dawids21 <dawid.stasiak21@gmail.com>
2020-08-17 13:26:44 +0200

    Create file for LoginCommand

---
+++ src/main/java/banking/LoginCommand.java
@@ -0,0 +1,10 @@
+package banking;
+
+public class LoginCommand implements Command {
+
+    @Override
+    public void execute() {
+        //TODO implement execute
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
