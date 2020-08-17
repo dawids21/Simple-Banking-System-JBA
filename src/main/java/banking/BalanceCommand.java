f7aadd726e6872fdc8376b64298b1f03f8be46a9
dawids21 <dawid.stasiak21@gmail.com>
2020-08-17 13:59:16 +0200

    Implement BalanceCommand

--- src/main/java/banking/BalanceCommand.java
+++ src/main/java/banking/BalanceCommand.java
@@ -2,9 +2,20 @@ package banking;
 
 public class BalanceCommand implements Command {
 
+    private final Account account;
+
+    public BalanceCommand(Account account) {
+        this.account = account;
+    }
+
     @Override
     public void execute() {
-        //TODO implement execute
-        throw new UnsupportedOperationException("Not implemented yet");
+        System.out.println();
+        if (account != null) {
+            System.out.println("Balance" + account.getBalance());
+        } else {
+            System.out.println("Balance: unknown");
+        }
+        System.out.println();
     }
 }
