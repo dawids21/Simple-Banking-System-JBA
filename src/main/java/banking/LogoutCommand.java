a86ed9112681acfd857aaa9ad1bdd5cea5139033
dawids21 <dawid.stasiak21@gmail.com>
2020-08-17 13:36:53 +0200

    Add LogoutCommand

---
+++ src/main/java/banking/LogoutCommand.java
@@ -0,0 +1,21 @@
+package banking;
+
+public class LogoutCommand implements Command {
+
+    private final Bank bank;
+
+    public LogoutCommand(Bank bank) {
+        this.bank = bank;
+    }
+
+    @Override
+    public void execute() {
+        System.out.println();
+        if (bank.logout()) {
+            System.out.println("You have successfully logged out!");
+        } else {
+            System.out.println("Error during logging out");
+        }
+        System.out.println();
+    }
+}
