62c0fa75a4220cd9ab908f924b721471ec2319df
dawids21 <dawid.stasiak21@gmail.com>
2020-08-17 13:17:42 +0200

    Implement CreateAccountCommand

--- src/main/java/banking/CreateAccountCommand.java
+++ src/main/java/banking/CreateAccountCommand.java
@@ -2,9 +2,20 @@ package banking;
 
 public class CreateAccountCommand implements Command {
 
+    private final Bank bank;
+
+    public CreateAccountCommand(Bank bank) {
+        this.bank = bank;
+    }
+
     @Override
     public void execute() {
-        //TODO implement execute
-        throw new UnsupportedOperationException("Not implemented yet");
+        var id = bank.createAccount();
+        var card = bank.getCard(id);
+        System.out.println("Your card has been created");
+        System.out.println("Your card number:");
+        System.out.println(card.getNumber());
+        System.out.println("Your card PIN:");
+        System.out.println(card.getPin());
     }
 }
