2cdd432f94581225ff3e40fb7402be8f0431bd14
dawids21 <dawid.stasiak21@gmail.com>
2020-08-17 13:14:41 +0200

    Create file for creating account command

---
+++ src/main/java/banking/CreateAccountCommand.java
@@ -0,0 +1,10 @@
+package banking;
+
+public class CreateAccountCommand implements Command {
+
+    @Override
+    public void execute() {
+        //TODO implement execute
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
