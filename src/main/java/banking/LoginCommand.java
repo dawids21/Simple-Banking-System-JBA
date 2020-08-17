2d70eaba97c8836e330ef9046cd5e73a18269053
dawids21 <dawid.stasiak21@gmail.com>
2020-08-17 13:29:38 +0200

    Implement LoginCommand

--- src/main/java/banking/LoginCommand.java
+++ src/main/java/banking/LoginCommand.java
@@ -1,10 +1,30 @@
 package banking;
 
+import java.util.Scanner;
+
 public class LoginCommand implements Command {
+
+    private final Scanner input;
+    private final Bank bank;
+
+    public LoginCommand(Scanner input, Bank bank) {
+        this.input = input;
+        this.bank = bank;
+    }
+
 
     @Override
     public void execute() {
-        //TODO implement execute
-        throw new UnsupportedOperationException("Not implemented yet");
+        System.out.println("Enter your card number:");
+        var cardNumber = input.nextLine();
+        System.out.println("Enter your PIN");
+        var cardPin = input.nextLine();
+        System.out.println();
+        if (bank.login(cardNumber, cardPin)) {
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Wrong card number or PIN!");
+        }
+        System.out.println();
     }
 }
