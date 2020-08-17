fa30882eaf431f1893d38f5a8335522236ea2333
dawids21 <dawid.stasiak21@gmail.com>
2020-08-17 10:53:33 +0200

    Implement CardGenerator

--- src/test/java/banking/RandomCardGeneratorTest.java
+++ src/test/java/banking/RandomCardGeneratorTest.java
@@ -5,62 +5,62 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CardGeneratorTest {
+class RandomCardGeneratorTest {
 
     private static final String IIN = "400000";
 
-    private CardGenerator cardGenerator;
+    private RandomCardGenerator randomCardGenerator;
 
     @BeforeEach
     void setUp() {
-        cardGenerator = new CardGenerator(IIN);
+        randomCardGenerator = new RandomCardGenerator(IIN);
     }
 
     @Test
     void generated_card_is_not_null() {
-        var card = cardGenerator.generate();
+        var card = randomCardGenerator.generate();
         assertNotNull(card);
     }
 
     @Test
     void card_number_begins_with_given_iin() {
-        var card = cardGenerator.generate();
+        var card = randomCardGenerator.generate();
         assertTrue(card.getNumber()
-                       .startsWith(CardGeneratorTest.IIN));
+                       .startsWith(RandomCardGeneratorTest.IIN));
     }
 
     @Test
     void card_number_contains_only_digits() {
-        var card = cardGenerator.generate();
+        var card = randomCardGenerator.generate();
         assertTrue(card.getNumber()
                        .matches("\\d+"));
     }
 
     @Test
     void card_number_has_16_chars() {
-        var card = cardGenerator.generate();
+        var card = randomCardGenerator.generate();
         assertEquals(16, card.getNumber()
                              .length());
     }
 
     @Test
     void two_generated_cards_should_be_unique() {
-        var card1 = cardGenerator.generate();
-        var card2 = cardGenerator.generate();
+        var card1 = randomCardGenerator.generate();
+        var card2 = randomCardGenerator.generate();
 
         assertNotEquals(card1, card2);
     }
 
     @Test
     void card_pin_contains_only_digits() {
-        var card = cardGenerator.generate();
+        var card = randomCardGenerator.generate();
         assertTrue(card.getPin()
                        .matches("\\d+"));
     }
 
     @Test
     void card_pin_has_16_chars() {
-        var card = cardGenerator.generate();
+        var card = randomCardGenerator.generate();
         assertEquals(4, card.getPin()
                             .length());
     }
