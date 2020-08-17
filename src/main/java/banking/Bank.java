f8d1ee30c65904bbba841ae5bb063f6f9057b4fe
dawids21 <dawid.stasiak21@gmail.com>
2020-08-17 10:54:07 +0200

    Write tests and stubs for Bank class

--- src/main/java/banking/Bank.java
+++ src/main/java/banking/Bank.java
@@ -2,4 +2,48 @@ package banking;
 
 public class Bank {
 
+    public Bank(CardGenerator cardGenerator) {
+        //TODO implement Bank
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public String createAccount() {
+        //TODO implement createAccount
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Account getAccount(String accountID) {
+        //TODO implement getAccount
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Card getCard(String accountID) {
+        //TODO implement getCard
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    private boolean validateCard(String cardNumber, String cardPin) {
+        //TODO implement validateCard
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public boolean login(String cardNumber, String cardPin) {
+        //TODO implement login
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Account getLoggedAccount() {
+        //TODO implement getLoggedAccount
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public boolean isLogged() {
+        //TODO implement isLogged
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public boolean logout() {
+        //TODO implement logout
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
--- src/test/java/banking/BankTest.java
+++ src/test/java/banking/BankTest.java
@@ -1,5 +1,154 @@
 package banking;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class BankTest {
 
+    static class TestCardGenerator implements CardGenerator {
+
+        private final String cardNumber;
+        private final String cardPin;
+
+        public TestCardGenerator(String cardNumber, String cardPin) {
+            this.cardNumber = cardNumber;
+            this.cardPin = cardPin;
+        }
+
+        @Override
+        public Card generate() {
+            return new Card(cardNumber, cardPin);
+        }
+    }
+
+    static class create_account {
+
+        private static final String NUMBER = "1234567890123456";
+        private static final String PIN = "1234";
+        Bank bank;
+        String accountID;
+
+        @BeforeEach
+        void setUp() {
+            bank = new Bank(new BankTest.TestCardGenerator(NUMBER, PIN));
+            accountID = bank.createAccount();
+        }
+
+        @Test
+        void creates_new_account() {
+            assertThrows(IllegalArgumentException.class,
+                         () -> bank.getAccount(accountID));
+        }
+
+        @Test
+        void new_account_has_balance_zero() {
+            assertEquals(0, bank.getAccount(accountID)
+                                .getBalance());
+        }
+
+        @Test
+        void new_card_is_generated_when_creating_new_account() {
+            assertNotNull(bank.getCard(accountID));
+        }
+    }
+
+    static class login {
+
+        private static final String NUMBER = "1234567890123456";
+        private static final String PIN = "1234";
+        Bank bank;
+
+        @BeforeEach
+        void setUp() {
+            bank = new Bank(new BankTest.TestCardGenerator(NUMBER, PIN));
+            bank.createAccount();
+        }
+
+        @Test
+        void returns_true_when_successfully_logged_in() {
+            assertTrue(bank.login(NUMBER, PIN));
+            assertTrue(bank.isLogged());
+        }
+
+        @Test
+        void returns_false_when_account_does_not_exist() {
+            assertFalse(bank.login("0000000000000000", "1234"));
+            assertFalse(bank.isLogged());
+        }
+
+        @Test
+        void returns_false_when_pin_is_not_correct() {
+            assertFalse(bank.login(NUMBER, "0000"));
+            assertFalse(bank.isLogged());
+        }
+    }
+
+    static class get_logged_account {
+
+        private static final String NUMBER = "1234567890123456";
+        private static final String PIN = "1234";
+        Bank bank;
+
+        @BeforeEach
+        void setUp() {
+            bank = new Bank(new BankTest.TestCardGenerator(NUMBER, PIN));
+            bank.createAccount();
+        }
+
+        @Test
+        void returns_logged_account_after_successful_login() {
+            bank.login(NUMBER, PIN);
+
+            var loggedAccount = bank.getLoggedAccount();
+            assertNotNull(loggedAccount);
+        }
+
+        @Test
+        void returns_null_after_unsuccessful_login() {
+            bank.login(NUMBER, "2222");
+
+            var loggedAccount = bank.getLoggedAccount();
+            assertNull(loggedAccount);
+        }
+
+        @Test
+        void returns_null_if_has_not_logged_in() {
+            var loggedAccount = bank.getLoggedAccount();
+            assertNull(loggedAccount);
+        }
+    }
+
+    static class logout {
+        private static final String NUMBER = "1234567890123456";
+        private static final String PIN = "1234";
+        Bank bank;
+
+        @BeforeEach
+        void setUp() {
+            bank = new Bank(new BankTest.TestCardGenerator(NUMBER, PIN));
+            bank.createAccount();
+        }
+
+        @Test
+        void returns_true_after_successful_logout() {
+            bank.login(NUMBER, PIN);
+
+            assertTrue(bank.logout());
+            assertFalse(bank.isLogged());
+        }
+
+        @Test
+        void returns_false_if_not_logged() {
+            assertFalse(bank.logout());
+        }
+
+        @Test
+        void sets_logged_account_to_null() {
+            bank.login(NUMBER, PIN);
+            bank.logout();
+            assertNull(bank.getLoggedAccount());
+        }
+    }
 }
