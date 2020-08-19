2c40efdbade6945902605935fcfa57a515fd2d3b
dawids21 <dawid.stasiak21@gmail.com>
2020-08-19 11:03:26 +0200

    Write tests for Luhn algorithm

--- src/main/java/banking/LuhnChecksumGenerator.java
+++ src/main/java/banking/LuhnChecksumGenerator.java
@@ -2,4 +2,13 @@ package banking;
 
 public class LuhnChecksumGenerator {
 
+    public LuhnChecksumGenerator(String number) {
+        //TODO implement LuhnChecksumGenerator
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public int generate() {
+        //TODO implement generate
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
--- src/test/java/banking/LuhnChecksumGeneratorTest.java
+++ src/test/java/banking/LuhnChecksumGeneratorTest.java
@@ -1,5 +1,31 @@
 package banking;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class LuhnChecksumGeneratorTest {
 
+    @Test
+    void throws_exception_if_card_number_len_is_less_than_15() {
+
+        assertThrows(IllegalArgumentException.class,
+                     () -> new LuhnChecksumGenerator("12345678901234"));
+    }
+
+    @Test
+    void throws_exception_if_card_number_len_is_more_than_15() {
+
+        assertThrows(IllegalArgumentException.class,
+                     () -> new LuhnChecksumGenerator("1234567890123456"));
+    }
+
+    @Test
+    void returns_checksum() {
+
+        var luhnChecksumGenerator = new LuhnChecksumGenerator("400000844943340");
+
+        assertEquals(3, luhnChecksumGenerator.generate());
+    }
 }
