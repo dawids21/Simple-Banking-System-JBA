1f66aeb568098de5e331f67f92e1bc684e7a6f47
dawids21 <dawid.stasiak21@gmail.com>
2020-08-25 07:53:43 +0200

    Add test in Luhn when doubled digit is over 10

--- src/test/java/banking/LuhnChecksumGeneratorTest.java
+++ src/test/java/banking/LuhnChecksumGeneratorTest.java
@@ -24,8 +24,15 @@ class LuhnChecksumGeneratorTest {
 
     @Test
     void returns_0_when_sum_is_divisible_by_10() {
-        var luhnChecksumGenerator = new LuhnChecksumGenerator("400000345009030");
+        var luhnChecksumGenerator = new LuhnChecksumGenerator("400000345009020");
 
         assertEquals(0, luhnChecksumGenerator.generate());
+    }
+
+    @Test
+    void subtract_9_from_doubled_digit_when_it_is_equal_to_or_higher_than_10() {
+        var luhnChecksumGenerator = new LuhnChecksumGenerator("400000000000005");
+
+        assertEquals(1, luhnChecksumGenerator.generate());
     }
 }
