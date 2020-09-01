be84e929343005bed21250eb31865a7b0e2519d5
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-09-01 12:47:01 +0200

    Create table if not exists in AccountsDatabase constructor

--- src/main/java/banking/AccountsDatabase.java
+++ src/main/java/banking/AccountsDatabase.java
@@ -1,21 +1,36 @@
 package banking;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class AccountsDatabase {
 
     private final String sqlUrl;
 
     public AccountsDatabase(String sqlUrl) {
         this.sqlUrl = sqlUrl;
+        try (var conn = DriverManager.getConnection(sqlUrl);
+                 var statement = conn.createStatement()) {
+            statement.execute(
+                     "CREATE TABLE IF NOT EXISTS accounts (\n" + "    id INTEGER,\n" +
+                     "    number TEXT,\n" + "    pin TEXT,\n" +
+                     "    balance INTEGER DEFAULT 0\n" + ");\n");
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void add(Account account) {
         //TODO implement add
         throw new UnsupportedOperationException("Not implemented yet");
     }
+
     public void getById(int id) {
         //TODO implement getById
         throw new UnsupportedOperationException("Not implemented yet");
     }
+
     public void update(int id, Account data) {
         //TODO implement update
         throw new UnsupportedOperationException("Not implemented yet");
