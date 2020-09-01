89ef72902311389ae55671768d94650ebff770de
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-09-01 12:39:27 +0200

    Add necessary methods to AccountsDatabase object

--- src/main/java/banking/AccountsDatabase.java
+++ src/main/java/banking/AccountsDatabase.java
@@ -7,4 +7,17 @@ public class AccountsDatabase {
     public AccountsDatabase(String sqlUrl) {
         this.sqlUrl = sqlUrl;
     }
+
+    public void add(Account account) {
+        //TODO implement add
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+    public void getById(int id) {
+        //TODO implement getById
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+    public void update(int id, Account data) {
+        //TODO implement update
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
