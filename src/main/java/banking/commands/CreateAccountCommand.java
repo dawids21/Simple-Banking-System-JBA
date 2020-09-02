131bb6c768c9a46126f5623b8c6042c688ddfaa1
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-09-02 07:44:52 +0200

    Null check in CreateAccountCommand

--- src/main/java/banking/commands/CreateAccountCommand.java
+++ src/main/java/banking/commands/CreateAccountCommand.java
@@ -12,7 +12,15 @@ public class CreateAccountCommand implements Command {
 
     @Override
     public void execute() {
-        var card = bank.createAccount().getCard();
+        var account = bank.createAccount();
+
+        if (account == null) {
+            System.out.println("Error during account creation");
+            System.out.println();
+            return;
+        }
+       
+        var card = account.getCard();
         System.out.println("Your card has been created");
         System.out.println("Your card number:");
         System.out.println(card.getNumber());
