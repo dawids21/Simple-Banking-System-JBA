76e73b577d281e66794ff5e3e2f2108ec7c5d42f
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-09-11 14:23:24 +0200

    write CloseLoggedAccountCommand

--- src/main/java/banking/commands/CloseLoggedAccountCommand.java
+++ src/main/java/banking/commands/CloseLoggedAccountCommand.java
@@ -1,9 +1,26 @@
 package banking.commands;
 
+import banking.Bank;
+import banking.exceptions.AccountNotFoundException;
+
 public class CloseLoggedAccountCommand implements Command {
+
+    private final Bank bank;
+
+    public CloseLoggedAccountCommand(Bank bank) {
+        this.bank = bank;
+    }
 
     @Override
     public void execute() {
-
+        var success = true;
+        try {
+            success = bank.closeAccount(bank.getLoggedAccount());
+        } catch (AccountNotFoundException e) {
+            e.printStackTrace();
+            success = false;
+        }
+        bank.logout();
+        System.out.println(success ? "The account has been closed!" : "Error while closing account");
     }
 }
